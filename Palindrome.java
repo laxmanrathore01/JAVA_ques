f7964bdb785fabb27dbbaddddc9f892a08e44524
Laxman Singh <130828363+laxmanrathore01@users.noreply.github.com>
2023-09-23 15:34:45 +0000

    save this

--- Palindrome.java
+++ Palindrome.java
@@ -1,11 +1,19 @@
 class Palindrome{  
  public static void main(String args[]){  
   int r,sum=0,temp;    
+<<<<<<< HEAD
   int n=454; 
   
   temp=n;    
   while(n>0){    
    r=n%10;  
+=======
+  int n=454;
+  
+  temp=n;    
+  while(n>0){    
+   r=n%10;   
+>>>>>>> 05c8527 (save this)
    sum=(sum*10)+r;    
    n=n/10;    
   }    
