6183e07aa2e7cfbe664345bd2a3e1ed06057b3c7
Laxman Singh <130828363+laxmanrathore01@users.noreply.github.com>
2023-09-23 19:40:54 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,9 @@
+public class OddEven {
+    public static void main(String args[]){
+        int a=5;
+        if (a%2==0)
+        System.out.println("number is even");
+        else
+        System.out.println("number is odd");
+    }
+}
