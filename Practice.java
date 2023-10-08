70c8a678aaa863110ec928470cad9df6f10cdf6a
Laxman Singh <130828363+laxmanrathore01@users.noreply.github.com>
2023-10-08 21:22:12 +0530

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,9 @@
+public class Practice{
+    public static void main(String args[]){
+        String name = "lsr";
+        int x=5;
+        char myValue=73;
+        System.out.println(myValue);
+
+    }
+}
