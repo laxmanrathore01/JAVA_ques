4935aa064decb072dad4cf2dc067db6cfbd90be1
Laxman Singh <130828363+laxmanrathore01@users.noreply.github.com>
2023-10-08 21:21:05 +0530

    Add files via upload

---
+++ Practice2.java
@@ -0,0 +1,35 @@
+public class Practice2 {
+    public static void main(String args[]){
+        // int first=36;
+        // // double second=56d;
+        // String name="fwfewfewfewfewfewfwefew";
+        // String lastName="iirereif";
+        // System.out.println(name + " " + lastName);
+        // // System.out.println(name.toUpperCase());
+        // // System.out.println(first);
+        // // System.out.println(second);
+        // System.out.println(Math.max(10,65));
+        // int myAge=15;
+        // int votingAge=18;
+        // if (myAge>=votingAge){
+        //     System.out.println("old enough to vote");
+        // }
+        // else{
+        //     System.out.println("not eligible");
+        // }
+        // int x = 70;
+        // int y = 70;
+        // if (x>y){
+        //     System.out.println("good");
+        // }
+        // else if (x<y){
+        //     System.out.println("nice");
+        // }
+        // else{
+        //     System.out.println("well");
+        // }
+        int name=50;
+        String mame=(name>80)? "good" : "nice";
+        System.out.println(mame);
+    }
+}
---
+++ Practice3.java
@@ -0,0 +1,42 @@
+public class Practice3 {
+    public static void main(String args[]){
+
+        // int a=5;
+        // while(a<10){
+        //     System.out.println(a);
+        //     a++;
+    //     // }
+    // for (int a = 5; a<10;a++){
+    //     System.out.println(a);
+    // }
+    // System.out.println("odd numbers are");
+    // for(int a=1;a<=20;a=a+2){
+        
+    //     System.out.println( a);
+
+        
+    // }
+    // for(int i=0;i<=3;i++){
+    //     System.out.println("outer loop"+i);
+    //     for (int j=0;j<=6;j++){
+    //         System.out.println("inner lopp"+j);
+    //     }
+    // }
+    // for (int i = 1 ; i<10;i++){
+    //     if (i==5){
+    //         break;
+    //     }
+    //     System.out.println(i);
+    // }
+
+    for (int a=2 ; a<20 ; a++){
+        if (a == 15){
+            continue;
+
+        }
+        System.out.println(a);
+    }
+    
+
+    }
+}
