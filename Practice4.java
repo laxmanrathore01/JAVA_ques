7f81ae1b4584fdd72982cf795057bcd6872b39a7
Laxman Singh <130828363+laxmanrathore01@users.noreply.github.com>
2023-10-09 22:54:54 +0530

    Add files via upload

---
+++ Practice4.java
@@ -0,0 +1,25 @@
+public class Practice4 {
+    public static void main(String args[]){
+        // String [] cars={"sodgnreig","iuibfaew","hfbe"};
+        // System.out.println(cars[1]);
+        // System.out.println(cars.length);
+        // for(int i=0 ;i< cars.length;i++){
+        //     System.out.println(cars[i]);
+
+        // }
+        
+        
+        // String[][] bike={{"rgrg","sdgfg","dss"},{"ewfgew","efe","efhge"}};
+        // System.out.println(bike[1][2]);
+
+        int [][] mark={{1,2,3,4},{11,12,13,14},{21,22,23,24,}};
+        for(int i=0;i<mark.length;i++){
+            for (int j=0;j<mark[i].length;j++){
+                System.out.println(mark[i][j]);
+               
+            }
+        }
+
+
+    }
+}
