681e67773cbbb52ebb7e969d5ce3b3e007cf756f
Laxman Singh <130828363+laxmanrathore01@users.noreply.github.com>
2023-10-27 14:53:27 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,14 @@
+public class StringMethods {
+    public static void main(String args[]){
+        String str= "Saket Sourav";
+        System.out.println(str.length());
+
+
+
+
+    String a1="Software";
+    String a2="Testing";
+    System.out.println(a1+a2);
+    System.out.println(a1.concat(a2));
+}
+}
